803cf5c89033f2cd7b5bdf08a43eeaee841f67d4
vitor.gama <vitorggam@hotmail.com>
2023-09-21 11:31:47 -0300

    Criando estâncias da classe (objetos!)

--- index.php
+++ index.php
@@ -3,10 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo</title>
+    <title>Exemplo 1</title>
 </head>
 <body>
-    <h1>PHP com POO</h1>
+    <h1>PHP com POO - Exemplo 1</h1>
     <hr>
+    <h2>Assuntos abordados:</h2>
+    <ul>
+        <li>Criação de classe</li>
+    </ul>
+    
+    
+    <?php
+    //importando a Classe
+    require_once "src/Cliente.php";
+
+    //Criando estâncias da classe (objetos!)
+    $clienteA = new Cliente;
+    $clienteB = new Cliente;
+    ?>
+
+    <pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
 </html>
