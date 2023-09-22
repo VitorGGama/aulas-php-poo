cbca89e9d4a85c5994b5b76db36b328802c85a0a
vitor.gama <vitorggam@hotmail.com>
2023-09-22 09:58:32 -0300

    setters e getters

--- index.php
+++ index.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< Updated upstream
-    <title>Exemplo 3</title>
+    <title>Exemplo 4</title>
 </head>
 <body>
     <h1>PHP com POO - Exemplo 4</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Encapsulamento      
-        </li>
-        <li>Uso do <code>$this</code> para acesso ás propriedades dentro do objeto</li>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibilidade 
+        (public, private e protected)</li>
     </ul>
 
-    <?php
-    require_once "src/Cliente.php";
-    $clienteA = new Cliente;
-    
-    //$clienteA->nome = "Vitor";
+<?php
+require_once "src/Cliente.php";
+$clienteA = new Cliente;
 
-  /*Usando o setter para atribuir um valor à propriedade privada "nome"*/ 
-  $clienteA->setNome("Fulano da Silva");
-    ?>
+/* O código abaixo dará erro pois
+a propriedade é privada! */
+//$clienteA->nome = "Tiago";
 
-    <pre><?=var_dump($clienteA)?></pre>
+/* Usando o setter para atribuir um valor 
+à propriedade privada "nome" */
+$clienteA->setNome("Fulano da Silva");
 
-    
+?>
+<!-- Usando o getter para ler/acessar dados privados -->
+<p>Nome: <?=$clienteA->getNome()?></p>
 
-   
+<pre> <?=var_dump($clienteA)?> </pre>
+
 
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,12 +1,16 @@
 <?php
-class Cliente
-{
-    //Propriedades (ou atributos)
+class Cliente {
+
     private string $nome;
-    private string $email;
+    private string $email; 
     private string $senha;
-    /*Métodos getters e setters */
-    public function setNome(string $nome): void {
+   
+    /* Métodos getters e setters */
+    public function setNome(string $nome):void {
         $this->nome = $nome;
+    }
+
+    public function getNome():string {
+        return $this->nome;
     }
 }
