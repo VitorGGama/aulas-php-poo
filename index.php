28eab65057aa9560450fccd50d494205bf500034
vitor.gama <vitorggam@hotmail.com>
2023-09-22 08:35:49 -0300

    saida de dados

--- index.php
+++ index.php
@@ -1,28 +1,61 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 1</title>
+    <title>Exemplo 2</title>
 </head>
+
 <body>
-    <h1>PHP com POO - Exemplo 1</h1>
+    <h1>PHP com POO - Exemplo 2</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação de classe</li>
+        <li>Acesso direto às propriedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de método</li>
     </ul>
-    
-    
+
     <?php
-    //importando a Classe
     require_once "src/Cliente.php";
 
-    //Criando estâncias da classe (objetos!)
     $clienteA = new Cliente;
     $clienteB = new Cliente;
+
+    // Acesso e atribuição
+    $clienteA->nome = "Vitor";
+    $clienteB->nome = "Vitória";
+
+    $clienteA->telefones = ["11-2135-0300", "11-96666-5555"];
+
+    $clienteA->email = "vitor@gmail.com";
+    $clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
     ?>
 
-    <pre><?=var_dump($clienteA, $clienteB)?></pre>
+    <hr>
+
+    <h2>Dados dos objetos (acesso e leitura)</h2>
+    <h3> <?= $clienteA->nome ?> </h3>
+    <p> E-mail: <?= $clienteA->email ?> </p>
+    <p> Telefones (acesso um por um):
+        <?= $clienteA->telefones[0] ?>, <?= $clienteA->telefones[1] ?>
+    </p>
+    <p>Telefones (convertendo para string):
+        <?= implode(", ", $clienteA->telefones) ?>
+    </p>
+    <p>Telefones (usando loop):</p>
+    <ul>
+        <?php foreach ($clienteA->telefones as $telefone) { ?>
+            <li><?= $telefone ?></li>
+        <?php } ?>
+    </ul>
+
+    <h3> <?= $clienteB->nome ?> </h3>
+
+
+    <pre><?= var_dump($clienteA, $clienteB) ?></pre>
+
 </body>
+
 </html>
