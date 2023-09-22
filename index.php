6d84984f30acfbd6730d152be9cd6779a4d2a1d8
vitor.gama <vitorggam@hotmail.com>
2023-09-22 09:29:48 -0300

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <title>Exemplo 3</title>
 </head>
 <body>
@@ -26,6 +27,21 @@
     ?>
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
     
+=======
+    <title>Exemplo 4 </title>
+</head>
+<body>
+    <h1>PHP com POO - Exemplo 4</h1>
+    <hr>
+    <h2>Assuntos abordados:</h2>
+    <ul>
+        <li>Encapsulamento</li>
+        <li>Modificadores de visibiladade</li>
+    </ul>
+    <?php
+    require_once "src"
+
+>>>>>>> Stashed changes
 
 </body>
 </html>
