6bb66de54cd1841ed0bf1b63c4a3bed7b8a7e681
vitor.gama <vitorggam@hotmail.com>
2023-09-28 10:57:08 -0300

    exibirDados

--- src/Cliente.php
+++ src/Cliente.php
@@ -3,42 +3,58 @@
 Não pode ser instanciada, ou seja, não é 
 possível criar um objeto a partir desta classe. Ela é usada apenas como modelo
 para subclasses com herança. */
-abstract class Cliente {
+abstract class Cliente
+{
 
     private string $nome;
-    private string $email; 
+    private string $email;
     private string $senha;
     private string $situacao = "a definir";
-   
+
+    public function exibirDados(): void
+    {
+        echo "<h3>Método exibirDados - Classe Cliente</h3>";
+        echo "<h4> $this->nome </h4>";
+        echo "<h4>" . $this->getNome() . "</h4>";
+
+        echo "<p>Situação: $this->situacao </p>";
+    }
+
     /* Métodos getters e setters */
-    public function setNome(string $nome):void {
+    public function setNome(string $nome): void
+    {
         $this->nome = $nome;
     }
 
-    public function getNome():string {
+    public function getNome(): string
+    {
         return $this->nome;
     }
 
-    public function setEmail(string $email):void {
+    public function setEmail(string $email): void
+    {
         $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
     }
 
-    public function getEmail():string {
+    public function getEmail(): string
+    {
         return $this->email;
     }
 
-    public function setSenha(string $senha):void {
+    public function setSenha(string $senha): void
+    {
         $this->senha = password_hash($senha, PASSWORD_DEFAULT);
     }
 
-    public function getSenha():string {
+    public function getSenha(): string
+    {
         return $this->senha;
     }
 
     /* Visibilidade protected
     Estes getters e setters poderão ser usados APENAS
     aqui (classe Cliente) e nas subclasses 
-    (PessoaFisica e PessoaJuridica) */ 
+    (PessoaFisica e PessoaJuridica) */
     protected function getSituacao(): string
     {
         return $this->situacao;
