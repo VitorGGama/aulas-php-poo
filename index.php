1046f87bbd37d1b64b352a6d9f73e8a2ab264389
vitor.gama <vitorggam@hotmail.com>
2023-09-29 08:47:55 -0300

    Namesspaces

--- index.php
+++ index.php
@@ -4,43 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 5</title>
+    <title>Exemplo 9</title>
 </head>
 
 <body>
-    <h1>PHP com POO - Exemplo 8</h1>
+    <h1>PHP com POO - Exemplo 9</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Propriedades e métodos estáticos</li>
-        <li>Acesso direto sem objetos/instancias</li>
-        <li>Uso do <code>self</code> para acesso (dentro da classe) aos recursos estáticos.</li>
+        <li><code>Namesspaces</code>: agrupamento de recursos (classes, funções, constantes)</li>
+        <li>Prevenção de conflitos entre classes de mesmo nome</li>
+        <li>Configurar e usar <code>Namesspaces</code> e alias</li>
     </ul>   
    
 
-    <?php
-    require_once "src/PessoaFisica.php";
-    $cliente1 = new PessoaFisica;
-    $cliente1->setNome("Astrogildo");
-    $cliente1->setIdade(75);
+   <?php
+   /* Só de fazer requice já dá erro no servidor, devido a terem o mesmo nome.*/
+   require_once "src/fornecedores/pagamento.php";
+   require_once "src/prestadores/pagamento.php";
 
-    $cliente2 = new PessoaFisica;
-    $cliente2->setNome("Enzo");
-    $cliente2->setIdade(18);
-
-
-    require_once "src/utilitarios.php";
-    Utilitarios::obterData();
-    ?>
-
-    <h2>Atendimentos do dia:
-    <?=Utilitarios::$dataAtual?></h2>
-
-    <h3>cliente: <?=$cliente1->getNome()?></h3>
-    <p>Tipo de atendimento: <?=Utilitarios::definirAtendimento( $cliente1->getIdade())?></p>
-    
-    <h3>cliente: <?=$cliente2->getNome()?></h3>
-    <p>Tipo de atendimento: <?=Utilitarios::definirAtendimento( $cliente2->getIdade())?></p>
+   ?>
     
 
 
